112690369714c3e21d0cef19434192131d3d7ec6
ramzan <ramzanthenolph@gmail.com>
2020-06-12 14:50:12 +0300

    add quotes to quote array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[]=[
+    new Quote('The greatest glory in living lies not in never falling, but in rising everytime we fall', 'Nelson Mandela', 'Founder'),
+    new Quote('The way to get started is to quit talking and begin doing', 'Walt Disney','Founder'),
+    new Quote('Life is what happens when you are busy making other plans', 'John Lennon', 'Founder'),
+    new Quote('Love the life you live. Live the life you love', 'Bob Marley', 'Founder'),
+  ];
   constructor() { }
 
   ngOnInit(): void {
