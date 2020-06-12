f97b286fc306a5c898a317a8932c874f1e04f4c0
ramzan <ramzanthenolph@gmail.com>
2020-06-12 14:26:52 +0300

    create class quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    showDescription: boolean;
+    constructor(public name: string, public author: string, public submitBy: string){
+        this.showDescription= false;
+    }
 }
