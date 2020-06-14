f14e5baeb83770366b161f05f29d006cb208f944
ramzan <ramzanthenolph@gmail.com>
2020-06-14 15:25:53 +0300

    add likes and dislikes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number, public name: string, public author: string, public submitBy: string){
+    constructor(public id: number, public name: string, public author: string, public submitBy: string, public likes: number, public dislikes: number){
         this.showDescription= false;
     }
 }
